4a0a5cd288122b951e1c14d321e7daabd4d99e77
Bram Vlerick <bram.vlerick@openpixelsystems.org>
2021-07-23 08:43:48 +0200

    logger: fix error in internal logger defines
    Signed-off-by: Bram Vlerick <bram.vlerick@openpixelsystems.org>

--- src/logger.c
+++ src/logger.c
@@ -21,9 +21,9 @@ char *log_strings[CFG_LOGGER_MSG_POOL_SIZE];
 #define LOGGER_INTERNAL_DEBUG(fmt_, ...) fprintf(stdout, "logger: (debug): "fmt_, ##__VA_ARGS__)
 #define LOGGER_INTERNAL_INFO(fmt_, ...) fprintf(stdout, "logger: (debug): "fmt_, ##__VA_ARGS__)
 #else
-#define LOGGER_ERR(fmt, ...) while(0){}
-#define LOGGER_DEBUG(fmt, ...) while(0){}
-#define LOGGER_INFO(fmt, ...) while(0){}
+#define LOGGER_INTERNAL_ERR(fmt, ...) while(0){}
+#define LOGGER_INTERNAL_DEBUG(fmt, ...) while(0){}
+#define LOGGER_INTERNAL_INFO(fmt, ...) while(0){}
 #endif /* CFG_LOGGER_DEBUG_INTERNALS */
 
 struct logger *logger_new(const struct logger_ops *ops)
@@ -33,7 +33,7 @@ struct logger *logger_new(const struct logger_ops *ops)
 #ifdef CFG_LOGGER_MSG_POOL
 	logger->cbuf = cbuffer_init_cbuffer(CFG_LOGGER_MSG_POOL_SIZE);
 	for (int i = 0; i < CFG_LOGGER_MSG_POOL_SIZE; i++) {
-		log_strings[i] = malloc((CFG_LOGGER_MSG_MAX_LEN + 1) * sizeof(char)); 
+		log_strings[i] = malloc((CFG_LOGGER_MSG_MAX_LEN + 1) * sizeof(char));
 		cbuffer_set_element(logger->cbuf, i, log_strings[i]);
 	}
 #endif /* CFG_LOGGER_MSG_POOL */
