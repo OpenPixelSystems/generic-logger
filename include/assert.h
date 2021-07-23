2d33edb01f1a4817f733cff64e3ab37262ad084e
Bram Vlerick <bram.vlerick@openpixelsystems.org>
2021-07-23 15:13:44 +0200

    logger: fix assert in crossbuild
    Signed-off-by: Bram Vlerick <bram.vlerick@openpixelsystems.org>

--- include/assert.h
+++ include/assert.h
@@ -1,22 +1,9 @@
 #ifndef _LOGGER_ASSERT_H_
 #define _LOGGER_ASSERT_H_
 
-#ifdef CFG_LOGGER_PRODUCTION
 #define assert(x) { \
 	if (!(x)) { \
 		while (1) {}; \
 	} }
-#else
-#include <stdio.h>
-#include <stdlib.h>
-
-#define assert(x) { \
-	if (!(x)) { \
-		printf("logger: (error): (%s:%s:%d): assert triggerd!\n",\
-			__FILE__,__FUNCTION__, __LINE__);\
-		exit(-1); \
-	} }
-
-#endif
 
 #endif /* _LOGGER_ASSERT_H_ */
